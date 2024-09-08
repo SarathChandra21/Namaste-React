9fc25b46f9caf7c4ca2d538a69ab6b01cdfb6f06
Sarath Chandra Dhanalakota <dschandra2003@gmail.com>
2024-09-08 15:58:36 +0530

    Episode 01

---
+++ App.js
@@ -0,0 +1,13 @@
+const para = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", {id: "child1"}, [
+        React.createElement("h1",{},"H1"),
+        React.createElement("h2",{},"H2"),
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1",{},"H1"),
+        React.createElement("h2",{},"H2"),
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(para);
