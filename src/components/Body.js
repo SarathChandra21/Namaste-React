472314d01916f2ed3e465e7e40217c7aadee5fa3
Sarath Chandra Dhanalakota <dschandra2003@gmail.com>
2024-10-12 16:39:44 +0530

    episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,66 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
+import { useEffect} from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
     // Correct casing for state variable
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+      fetchData();
+    }, [])
     
-    return (
+    const fetchData = async ()=>{
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+        
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    return listOfRestaurants.length === 0? <Shimmer/>: (
         <div className="body">
+            <input 
+                type="text" 
+                className="search-box" 
+                value={searchText}
+                onChange ={
+                    (e)=>{
+                        setSearchText(e.target.value);
+                    }
+                }
+            />
+            <button 
+                className="search-btn"
+                onClick ={
+                    ()=>{
+                        const filteredList = listOfRestaurants.filter(
+                            (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilteredRestaurants(filteredList);
+                    }
+                }
+            >
+                    Search
+            </button>
             <button className="filter-btn" onClick={() => {
-                const filteredList = listOfRestaurants.filter((res) => res.data.avgRating > 4);
-                setListOfRestaurants(filteredList);
+                const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+                setFilteredRestaurants(filteredList);
             }}>
                 Top restaurants
             </button>
             <div className="res-container">
-                {listOfRestaurants.map((restaurant) => (
-                    <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+                {filteredRestaurants.map((restaurant) => (
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 ))}
             </div>
         </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
-export const CDN_URL = "https://www.licious.in/blog/wp-content/uploads/2022/06/chicken-hyderabadi-biryani-01.jpg";
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
